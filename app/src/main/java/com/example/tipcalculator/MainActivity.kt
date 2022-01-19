010ee3ef977eb8e50ca55458c26fad2d131598fc
Surprise <mesanwoolasurprise303>
2022-01-18 18:59:34 -0800

    Created some functions for the app logic

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -29,15 +29,45 @@ class MainActivity : AppCompatActivity() {
         seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 tipPercentage.text = "$p1%"
+                computeTipAndTotal()
                 Log.i(TAG, "onProgressChange $p1")
             }
             override fun onStartTrackingTouch(p0: SeekBar?) {}
             override fun onStopTrackingTouch(p0: SeekBar?) {}
         })
 
+        editBaseText.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG, "afterTextChange$p0")
+                computeTipAndTotal()
+            }
+        })
+
     }
 
-//    private fun computeTipAndTotal() {
-//
-//    }
+    private fun computeTipAndTotal() {
+        // Get the value of the base tip
+        val seekBarTip: SeekBar = findViewById(R.id.seek_bar_tip_percentage)
+        val editBaseText: EditText = findViewById(R.id.edit_text_base)
+        val tipAmount: TextView = findViewById(R.id.text_tip_result)
+        val total: TextView = findViewById(R.id.text_total_result)
+        if (editBaseText.text.isEmpty()) {
+            tipAmount.text = ""
+            total.text = ""
+            return
+        }
+        val baseAmount = editBaseText.text.toString().toDouble()
+        val tip = seekBarTip.progress
+        val tipTotal = baseAmount * tip / 100
+        tipAmount.text = "%.2f".format(tipTotal)
+
+        val allTotal = baseAmount + tipTotal
+        total.text = "%.2f".format(allTotal)
+
+    }
+
 }
